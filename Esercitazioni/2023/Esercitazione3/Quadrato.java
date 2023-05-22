dd2c6f759b354f7b2b00f00e3f1c4ce0a78311d3
Samuele <62257781+bagalasamuele@users.noreply.github.com>
2023-05-22 18:55:24 +0200

    Esercitazione3: solve first point

--- Esercitazioni/2023/Esercitazione3/Quadrato.java
+++ Esercitazioni/2023/Esercitazione3/Quadrato.java
@@ -1,23 +1,32 @@
+
 // Quadrato.java  quadrato = una possibile forma di una Figura
 // Definiamo Quadrato come una sottoclasse di Figura
-import java.awt.*;    //Abstract Window Toolkit
+import java.awt.*; //Abstract Window Toolkit
 import javax.swing.*; //estensione di awt per interfacce grafiche
+
 public class Quadrato extends Figura {
-  //Un quadrato e' definito dal suo lato
+  // Un quadrato e' definito dal suo lato
   private int lato;
+  private int x;
+  private int y;
+  private Color color;
+
   // COSTRUTTORE di un quadrato
-  public Quadrato(int lato){ this.lato = lato; }
-  // OVERRIDE: RI-definiamo il metodo draw (vuoto in Figura)
-  // per disegnare una figura nel caso di un quadrato.
-  // Scegliamo il quadrato centrato nell'origine e orizzontale.
-  // Scegliamo il colore arancio per le prossime linee in g.
-  public void draw(Graphics g){
-   g.setColor(Color.orange);
-   int m = lato / 2;
-   g.drawLine( m,  m, -m,  m);   //disegno primo   lato in g
-   g.drawLine(-m,  m, -m, -m);   //disegno secondo lato in g
-   g.drawLine(-m, -m,  m, -m);   //disegno terzo   lato in g
-   g.drawLine( m, -m,  m,  m);   //disegno quarto  lato in g
+  public Quadrato(int x, int y, int lato, Color color) {
+    this.lato = lato;
+    this.x = x;
+    this.y = y;
+    this.color = color;
+  }
+
+  public void draw(Graphics g) {
+    // g.setLocation(x,y);
+    g.setColor(color);
+    int m = lato / 2;
+    g.drawLine( x+m, y+m, x-m, y+m); //disegno primo lato su g
+    g.drawLine( x-m, y+m, x-m, y-m); //disegno secondo lato su g
+    g.drawLine( x-m, y-m, x+m, y-m); //disegno terzo lato su g
+    g.drawLine( x+m, y-m, x+m, y+m); //disegno quarto lato su g
   }
 
 }

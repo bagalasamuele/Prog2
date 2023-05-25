b6bd5cbdc7f051de0de8f8d5b4156732e4e437c8
Samuele <62257781+bagalasamuele@users.noreply.github.com>
2023-05-25 12:19:18 +0200

    GenericPair: add main class for make tests

---
+++ tests/GenericPair/main.java
@@ -0,0 +1,7 @@
+public class main {
+
+    public static void main(String args[]) {
+        GenericPair<String, Integer> test = new GenericPair<>("Ciao", 1);
+        System.out.println(test);
+    }
+}
